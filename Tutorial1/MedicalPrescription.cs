e15f361ad88b8596eb3e4d46158fe497f53e2086
bullhugger <zhengsenj@gmail.com>
2017-11-22 12:38:57 +0800

    Medical Prescription class

---
+++ Tutorial1/MedicalPrescription.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OverSurgeryUnitTest
+{
+    public class MedicalPrescription
+    {
+        string prescriptionID;
+        string patientID;
+        string gpID;
+        string patientDisease;
+        int patientCondition;
+        string drugPrescribed;
+
+        public string PrescriptionID
+        {
+            get
+            {
+                return prescriptionID;
+            }
+
+            set
+            {
+                prescriptionID = value;
+            }
+        }
+
+        public string PatientID
+        {
+            get
+            {
+                return patientID;
+            }
+
+            set
+            {
+                patientID = value;
+            }
+        }
+
+        public string GpID
+        {
+            get
+            {
+                return gpID;
+            }
+
+            set
+            {
+                gpID = value;
+            }
+        }
+
+        public string PatientDisease
+        {
+            get
+            {
+                return patientDisease;
+            }
+
+            set
+            {
+                patientDisease = value;
+            }
+        }
+
+        public int PatientCondition
+        {
+            get
+            {
+                return patientCondition;
+            }
+
+            set
+            {
+                patientCondition = value;
+            }
+        }
+
+        public string DrugPrescribed
+        {
+            get
+            {
+                return drugPrescribed;
+            }
+
+            set
+            {
+                drugPrescribed = value;
+            }
+        }
+    }
+}
---
+++ Tutorial1/MedicalPrescriptionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace OverSurgeryUnitTest
+{
+    public class MedicalPrescriptionHandler
+    {
+        public int addNewMP(MySqlConnection conn, MedicalPrescription mp)
+        {
+            string sql = "INSERT INTO medical_prescription (prescriptionID, patientID, gpID, patientDisease, patientCondition, drugPrescribed) " + " VALUES ('" + mp.PrescriptionID + "'," + mp.PatientID + ", '" + mp.GpID + "," + mp.PatientDisease + "'," + mp.PatientCondition + "'," + mp.DrugPrescribed + "')";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            return cmd.ExecuteNonQuery();
+        }
+    }
+}
